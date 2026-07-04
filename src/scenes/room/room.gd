a77658fc9eb98750e4da726f5d1e8cc0af32a1ef
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-07-04 13:30:17 +0200

    add basic mecanic

--- src/scenes/room/room.gd
+++ src/scenes/room/room.gd
@@ -1,11 +1,31 @@
 extends Node
 
+@export var MAX_TIME := 300.0
 
-# Called when the node enters the scene tree for the first time.
+var bit : int
+var slapped : int
+var t : float
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	$Hunter.connect("bit", bite)
+	$Hunter.connect("slapped", slap)
 
+func bite() -> void:
+	bit += 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func slap() -> void:
+	slapped += 1
+
 func _process(delta: float) -> void:
-	pass
+	t += delta
+	if bit + slapped > 0 or t > MAX_TIME:
+		end_fight()
+
+func start_fight() -> void:
+	bit = 0
+	slapped = 0
+
+func end_fight() -> void:
+	var winner_name = "Mosquito" if bit > slapped else "Hunter"
+	print("%s win !" % [winner_name])

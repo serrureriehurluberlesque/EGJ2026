b5fee47c4ced712e9bfec7d54eb5abf46cbfcd41
Elsa Weber <elsa.weber0@gmail.com>
2026-07-04 12:15:37 +0200

    attack with direction

--- src/scenes/hunter/hunter.gd
+++ src/scenes/hunter/hunter.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
+const ATTACK_DELTA = 25 # px
+
 @export var speed = 200
+@export var direction: Vector2 = Vector2(1.0, 0.0)
 
 var is_attacking = false
 
 func get_input():
 	var input_direction = Input.get_vector("hunter_left", "hunter_right", "hunter_up", "hunter_down")
 	velocity = input_direction * speed
+	
+	if input_direction:
+		direction = input_direction
 
 func _physics_process(delta):
 	if not is_attacking:
@@ -15,8 +21,12 @@ func _physics_process(delta):
 
 func _input(event):
 	if Input.is_action_pressed("hunter_attack"):
-		%Attack.show()
-		is_attacking = true
-		await get_tree().create_timer(1).timeout
-		%Attack.hide()
-		is_attacking = false
+		attack()
+		
+func attack():
+	%Attack.position = ATTACK_DELTA * direction
+	%Attack.show()
+	is_attacking = true
+	await get_tree().create_timer(1).timeout
+	%Attack.hide()
+	is_attacking = false

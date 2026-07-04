db2a010bbdd643dcb1ef2bb3d7876acff957fec2
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-07-04 12:20:13 +0200

    update movement hunter

--- src/scenes/hunter/hunter.gd
+++ src/scenes/hunter/hunter.gd
@@ -3,16 +3,19 @@ extends CharacterBody2D
 const ATTACK_DELTA = 25 # px
 
 @export var speed = 200
+@export var acceleration_factor = 0.75
+var previous_velocity := Vector2(0, 0)
 @export var direction: Vector2 = Vector2(1.0, 0.0)
 
 var is_attacking = false
 
 func get_input():
 	var input_direction = Input.get_vector("hunter_left", "hunter_right", "hunter_up", "hunter_down")
-	velocity = input_direction * speed
+	velocity = (1 - acceleration_factor) * (input_direction * speed) + acceleration_factor * previous_velocity
+	previous_velocity = velocity
 	
 	if input_direction:
-		direction = input_direction
+		direction = velocity.limit_length()
 
 func _physics_process(delta):
 	if not is_attacking:

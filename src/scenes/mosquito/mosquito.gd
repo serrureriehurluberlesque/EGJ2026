177f032caac0b51b521c0d1ea62fa281f19959e1
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-07-04 12:06:47 +0200

    add boundary and smooth mosquito movement

--- src/scenes/mosquito/mosquito.gd
+++ src/scenes/mosquito/mosquito.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var acceleration_factor = 0.9
+var previous_velocity := Vector2(0, 0)
 
 func get_input():
 	var input_direction = Input.get_vector("ms_left", "ms_right", "ms_up", "ms_down")
-	velocity = input_direction * speed
+	velocity = (1 - acceleration_factor) * (input_direction * speed) + acceleration_factor * previous_velocity
+	previous_velocity = velocity
 
 func _physics_process(delta):
 	get_input()

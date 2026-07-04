44aab08f2568d4c0ce7d331b7f4e9c9d9a8e8c1d
Elsa Weber <elsa.weber0@gmail.com>
2026-07-04 12:04:53 +0200

    setup hunter attack

--- src/scenes/hunter/hunter.gd
+++ src/scenes/hunter/hunter.gd
@@ -2,10 +2,21 @@ extends CharacterBody2D
 
 @export var speed = 200
 
+var is_attacking = false
+
 func get_input():
 	var input_direction = Input.get_vector("hunter_left", "hunter_right", "hunter_up", "hunter_down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	if not is_attacking:
+		get_input()
+		move_and_slide()
+
+func _input(event):
+	if Input.is_action_pressed("hunter_attack"):
+		%Attack.show()
+		is_attacking = true
+		await get_tree().create_timer(1).timeout
+		%Attack.hide()
+		is_attacking = false

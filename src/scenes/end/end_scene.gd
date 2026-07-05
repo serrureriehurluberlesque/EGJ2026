baa139c7aed8d810a161f5aab6b94420c5482413
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-07-05 10:06:29 +0200

    update space for controller

--- src/scenes/end/end_scene.gd
+++ src/scenes/end/end_scene.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	get_node("%" + Globals.winner_name + "Wins").show()
 
 func _input(event):
-	if Input.is_key_pressed(KEY_SPACE):
+	if Input.is_action_pressed("space"):
 		%HunterWins.hide()
 		%MosquitoWins.hide()
 		get_tree().change_scene_to_file("res://scenes/room/room.tscn")
